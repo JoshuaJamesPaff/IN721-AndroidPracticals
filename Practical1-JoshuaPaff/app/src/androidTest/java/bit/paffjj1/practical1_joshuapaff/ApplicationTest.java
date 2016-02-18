9f1d22101a191b717213b4ebf8162899076ad16c
JoshuaJamesPaff <joshuapaff@hotmail.com>
2016-02-18 17:05:38 +1300

    Practical 1 Finished
    not sure if correct gitignore

---
+++ Practical1-JoshuaPaff/app/src/androidTest/java/bit/paffjj1/practical1_joshuapaff/ApplicationTest.java
@@ -0,0 +1,13 @@
+package bit.paffjj1.practical1_joshuapaff;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ Practical1-JoshuaPaff/app/src/main/java/bit/paffjj1/practical1_joshuapaff/MainActivity.java
@@ -0,0 +1,13 @@
+package bit.paffjj1.practical1_joshuapaff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
